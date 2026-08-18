ba9b2caf0f9e57eccb1ec311864bd0b4bc338d13
Sander van Nielen <s.s.van.nielen@cml.leidenuniv.nl>
2026-08-18 10:51:04 +0200

    Serializers and KPI calculations for KAM user

--- mysite/dss/urls.py
+++ mysite/dss/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import ExperimentComparisonInitView, McdaWizardView, ExperimentResultsView
+from .views import (
+    ExperimentComparisonInitView, KamComparisonInitView,
+    McdaWizardView, ExperimentResultsView,
+)
 
 urlpatterns = [
-    path("experiments/", ExperimentComparisonInitView.as_view(), name="experiment-init"),
+    path("experiments/", ExperimentComparisonInitView.as_view(), name="pd-init"),
+    path("welding-stations/", KamComparisonInitView.as_view(), name="kam-init"),
     path("<int:session_id>/step/<int:step>/", McdaWizardView.as_view(), name="wizard"),
     path("results/<int:session_id>/", ExperimentResultsView.as_view(), name="results"),
 ]
